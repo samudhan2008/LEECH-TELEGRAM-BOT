53e39cea70cb820909e8d06f890f939decfb254d
samudhan2008 <159536401+samudhan2008@users.noreply.github.com>
2024-04-26 18:40:49 +0530

    __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -7,7 +7,7 @@ import logging
 import os
 import sys
 import traceback
-
+const port = process.env.PORT || 4000;
 from pyrogram import Client, filters
 from pyrogram.handlers import CallbackQueryHandler, MessageHandler
 
